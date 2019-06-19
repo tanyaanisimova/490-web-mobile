0aca23a5b77764087a5f33dacb34d737ac69f868
Tanya Anisimova <anisimova.tanya07@gmail.com>
2019-06-19 11:09:48 -0500

    ICP 5 screenshots

--- Lab 1/source/Snake/snake.js
+++ Lab 1/source/Snake/snake.js
@@ -1,37 +1,4 @@
-// angular.module('Snake', [])
-//     .controller('Ctrl', function ($scope) {
-//         $scope.size = 6;
-//         $scope.bestScore = 0;
-//         $scope.score = 0;
-//         $scope.body = [];
-//         $scope.direction = 0;
-//
-//         $scope.newTarget = function () {
-//             $scope.target = getBox()
-//         };
-//
-//         $scope.getBox = function () {
-//             return {x: Math.random() * $scope.size, y: Math.random() * $scope.size};
-//         };
-//
-//         $scope.updateScore = function () {
-//             if ($scope.score > $scope.bestScore) {
-//                 $scope.bestScore = $scope.score
-//             }
-//         };
-//
-//         $scope.newGame = function () {
-//            $scope.updateScore();
-//            $scope.score = 0;
-//            $scope.body = new Array(getBox());
-//            $scope.target = getBox();
-//            //start timer
-//         };
-//     });
-
-
-angular.module('ngSnake', [])
-
+angular.module('Snake', [])
     .controller('snakeCtrl', function($scope, $timeout, $window) {
         const BOARD_SIZE = 20;
 
