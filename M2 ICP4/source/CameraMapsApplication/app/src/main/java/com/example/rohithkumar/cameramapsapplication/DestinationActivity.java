a148bfb4214f619d36ddbddcdae5fddb79f30e5a
Tanya Anisimova <anisimova.tanya07@gmail.com>
2019-07-16 12:26:56 -0500

    Sensor and destination acitvities WIP

--- M2 ICP4/source/CameraMapsApplication/app/src/main/java/com/example/rohithkumar/cameramapsapplication/DestinationActivity.java
+++ M2 ICP4/source/CameraMapsApplication/app/src/main/java/com/example/rohithkumar/cameramapsapplication/DestinationActivity.java
@@ -1,31 +1,22 @@
 package com.example.rohithkumar.cameramapsapplication;
 
-import android.*;
-import android.content.Context;
-import android.content.pm.PackageManager;
-import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
-import android.location.LocationListener;
 import android.location.LocationManager;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
-import android.location.Geocoder;
+import android.support.v4.app.FragmentActivity;
 import android.util.Log;
-import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.util.List;
 
-public class MyMapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class DestinationActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     public Geocoder geocoder;
@@ -34,7 +25,7 @@ public class MyMapsActivity extends FragmentActivity implements OnMapReadyCallba
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_my_maps);
+        setContentView(R.layout.activity_destination);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
--- M2 ICP4/source/CameraMapsApplication/app/src/main/java/com/example/rohithkumar/cameramapsapplication/MainActivity.java
+++ M2 ICP4/source/CameraMapsApplication/app/src/main/java/com/example/rohithkumar/cameramapsapplication/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.rohithkumar.cameramapsapplication;
 
+import android.Manifest;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -15,6 +16,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String[] perms= { Manifest.permission.ACCESS_FINE_LOCATION };
+        requestPermissions(perms, 1);
     }
 
     public void onPhotoClick(View v) {
@@ -26,6 +29,18 @@ public class MainActivity extends AppCompatActivity {
     public void onLocationClick(View v) {
         //This code redirects to the photo activity.
         Intent redirect = new Intent(MainActivity.this, MyMapsActivity.class);
+        startActivity(redirect);
+    }
+
+    public void onSensorClick(View v) {
+        //This code redirects to the sensor activity.
+        Intent redirect = new Intent(MainActivity.this, SensorActivity.class);
+        startActivity(redirect);
+    }
+
+    public void onDestinationClick(View v) {
+        //This code redirects to the destination activity.
+        Intent redirect = new Intent(MainActivity.this, DestinationActivity.class);
         startActivity(redirect);
     }
 }
